a96cec9c8dbe91100e51e768039514b352d4b41e
Lubembe Michael <lubemb@gmail.com>
2022-12-28 13:18:36 +0300

    added a long int and a wchar_t

--- 3_sizeof.c
+++ 3_sizeof.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <wchar.h>
 
 /**
  * programatically compute the size of
@@ -8,5 +9,6 @@
 int main(void)
 {
 	printf("Size of int: %lu bytes\nSize of char: %lu bytes\n", sizeof(int), sizeof(char));
+	printf("Size of long int: %lu bytes\nSize of large char: %lu bytes\n", sizeof(long int), sizeof(wchar_t));
 	return (0);
 }
